f5ca8a9c36b826275075b5f639db4572cdae4f2a
alexpanamaryov <alp@norse.digital>
2016-12-14 02:48:15 +0200

    ProjectScreen

--- js/tabs.js
+++ js/tabs.js
@@ -24,3 +24,30 @@ function openTab(evt, tabName) {
     /*evt.target.parentNode.className += " active-tab";*/
     evt.currentTarget.parentNode.className += " active-tab";
 }
+
+function openHorizontalTab(evt, tabName) {
+    "use strict";
+
+    var tabPanels = document.getElementsByClassName("horizontal-tab-panel");
+
+    var tablinks = document.getElementsByClassName("horizontal-tablinks");
+
+    var tabs = document.getElementsByClassName("horizontal-tab");
+
+
+
+    /* tab inactivation */
+
+    for(var i=0; i<tabPanels.length; i++) {
+        tabPanels[i].style.display = "none";
+        tabs[i].className = tabs[i].className.replace(" active-horizontal-tab", "");
+        tablinks[i].className = tablinks[i].className.replace(" active-horizontal-tab-link", "");
+    }
+
+    /* tab activation */
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active-horizontal-tab-link";
+    /*evt.target.parentNode.className += " active-tab";*/
+    evt.currentTarget.parentNode.className += " active-horizontal-tab";
+}
